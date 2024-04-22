e36961c5bf3fccdca411d82de65d926c109c1a03
jasskhalsa <35370424+jasskhalsa@users.noreply.github.com>
2024-04-22 16:07:59 +0000

    323

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+
+console.log(3+3)
+
+
+
+let name1 = "Jagdeep"
+console.log(typeof(name1))
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+

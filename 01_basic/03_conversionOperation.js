87be4b7b7b5531d530c4e18725f67db7b31fdb28
Jagdeep Singh <jagdeep.singh85@live.com>
2024-04-23 21:46:58 +0530

    learning on 23 Apr 2024

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = null;
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false;
+// "" => false
+// "name" => true

dc0ca2268f5120ab5944cad66e251426982486bc
Jagdeep Singh <jagdeep.singh85@live.com>
2024-04-24 22:25:39 +0530

    24 Apr 2024

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -6,11 +6,17 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false;
 // "" => false
 // "name" => true
+
+// ****************** Operations ********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
